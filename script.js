9300e1e8742343bc81523eb5dd0fb068e073be49
Manjon19 <64155507+Manjon19@users.noreply.github.com>
2023-02-07 21:32:45 +0100

    Fetch semicompleto
    Llamadas a la api casi completado

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//api https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/
+const buscar = document.getElementById("buscar");
+let arrayAPI = [];
+let locationList;
+getLocationList();
+buscar.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    console.log(locationList);
+    let locationID = locationList.find(element => element.Municipio == buscar.value).IDMunicipio;
+    getDataAPI(locationID);
+  }
+})
+
+
+async function getLocationList() {
+  await fetch("https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/Municipios/")
+    .then(response => response.json())
+    .then(data => {
+      locationList = data;
+    })
+}
+async function getDataAPI(locationId) {
+  await fetch("https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipio/" + locationId)
+    .then(response => response.json()).then(
+      data => {
+        console.log(data.ListaEESSPrecio);
+      }
+    )
+}
