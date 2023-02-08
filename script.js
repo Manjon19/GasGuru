dc347ff0e80a53a50a9a532ee76f1f97783ebe4b
Manjon19 <davidmanjon500@gmail.com>
2023-02-08 19:20:29 +0100

    CCAA added

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 const buscar = document.getElementById("buscar");
+const ca = document.getElementById("CCAA");
 let arrayAPI = [];
 let locationList;
+let ccaaList;
 getLocationList();
-
+getCCAAList();
+let gen = "";
+ccaaList.forEach(element => {
+  gen += element.CCAA + "<br>";
+})
+ca.innerHTML = gen
 buscar.addEventListener("keypress", (event) => {
   if (event.key == "Enter") {
-    console.log(locationList);
     let locationID = locationList.find(element => element.Municipio == buscar.value).IDMunicipio;
-    getDataAPI(locationID);
+
+    let munGasList = getDataAPI(locationID);
+    munGasList.forEach(elemento => {
+      createGasCard(elemento);
+    });
   }
 })
 
-
 async function getLocationList() {
-  await fetch("./Municipios.json")
+  await fetch("./JSONS/Municipios.json")
     .then(response => response.json())
     .then(data => {
       locationList = data;
     })
+}
+async function getCCAAList() {
+  await fetch("./JSONS/CCAA.json")
+    .then(response => response.json())
+    .then(data => {
+      ccaaList = data;
+    })
+}
+async function getCAMun(ccaaID) {
+  await fetch("https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/MunicipiosPorProvincia/" + ccaaID).then(response => response.json()).then(
+    data => {
+      console.log(data);
+    }
+  )
 }
 async function getDataAPI(locationId) {
   await fetch("https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipio/" + locationId)
